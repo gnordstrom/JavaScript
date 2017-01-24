5f20960d803db2486f42fdb02b3b04820601eb6c
Gustav Nordstrom <gnordstrom@users.noreply.github.com>
2017-01-24 09:44:26 -0700

    Added basic folder and prob 1, solved abcheck

---
+++ basic/abcheck.js
@@ -0,0 +1,14 @@
+// Write a function called ABCheck that takes a string parameter and return true
+// if the characters a and b are separated by exactly 3 places anywhere
+// in the string at least once (ie. "lane borrowed" would result in true
+// because there is exactly three characters between a and b).
+// Otherwise return false.
+
+function ABCheck(str) {
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] === 'a' && str[i+4] === 'b'|| str[i] === 'b' && str[i + 4] === 'a') {
+            return true;
+        }
+    }
+    return false;
+}

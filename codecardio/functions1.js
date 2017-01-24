f027684e693f4b9eee554601646e1b9082fbded3
Gustav Nordstrom <gnordstrom@users.noreply.github.com>
2017-01-24 11:46:29 -0700

    Added codecardio folder and completed functions1.js

---
+++ codecardio/functions1.js
@@ -0,0 +1,39 @@
+// Create a function called sayHi that returns the string 'hi'
+function sayHi() {
+    return "hi";
+}
+
+
+// Make a function called sayHi that takes in a message parameter and then returns the message
+function sayHi(message) {
+    return message;
+}
+
+// Add a variable named total that is equal to num1 + num2
+function addNums(num1, num2) {
+    // code here 
+        var total = num1 + num2;
+    return total;
+}
+
+// Create a function called multiply that take two numbers as parameters. 
+// Inside the function body create a variable called total that is equal to the two parameters 
+// multiplied together, then return that variable
+function multiply(a, b) {
+    var total = a * b;
+    return total;
+}
+
+// Return true if num1 is equal to num2
+function isEqual(num1,num2) {
+    if (num1 === num2) {
+        return true;
+    }
+}
+
+// Write a if statement that will check if num1 is equal to num2, if both numbers are equal then return true
+function isEqual(num1,num2) {
+    if (num1 === num2) {
+        return true;
+    }
+}
